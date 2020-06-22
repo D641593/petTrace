c8e5b2a38f2080b8152dd8c5098c1e8c4b5bc36e
D641593 <D0641593@o365.fcu.edu.tw>
2020-06-22 19:30:51 +0800

    catETest & catTTest

---
+++ catTTest.py
@@ -0,0 +1,21 @@
+import unittest
+from catTrace import *
+from catEmulator import *
+import time
+
+class catTraceTest(unittest.TestCase):
+    catT = catTraceT()
+    catE = catEmulatorT()
+
+    def test_run(self):
+        self.catE.start()
+        self.catT.start()
+        anchor_DisQ.put([50,50,50,50])
+        anchor_DisQ.put([141,123,208,156])
+        time.sleep(2)
+        self.catE.flag = False
+        self.catE.join()
+        self.catT.flag = False
+        self.catT.join()
+
+unittest.main()
